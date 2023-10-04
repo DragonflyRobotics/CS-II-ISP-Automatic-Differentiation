957418a2dbd0ef3f3f1054398c96ee7eaebdd7ce
Krishna Shah <kshahusa@gmail.com>
2023-10-04 10:57:41 -0500

    Class doesn't work

---
+++ CalCoolUs/preprocess.py
@@ -0,0 +1,121 @@
+import random, string
+import networkx as nx
+from matplotlib import pyplot as plt
+
+class ShuntingYard:
+	def __init__(self):
+		self.operations = ["+", "-", "/", "*", "^"]
+
+	def tokenize(self, string):
+		r = [""]
+		for i in string:
+			if (i.isdigit() or i == ".") and self.isfloat(r[-1]):
+				r[-1] = r[-1] + i
+			else:
+				r.append(i)
+		return r[1:]
+
+	def isfloat(self, number):
+		try:
+			float(number)
+			return True
+		except:
+			return False
+
+	def isValue(self, number):
+		return self.isfloat(number) or number == 'x' or isinstance(number, str)
+
+	def precedence(self, operator):
+		match operator:
+			case "+":
+				return 1
+			case "-":
+				return 1
+			case "*":
+				return 2
+			case "/":
+				return 2
+			case "^":
+				return 3
+		return 0
+
+	def getPostfix(self, diffEquation):
+		diffEquation = diffEquation.replace(" ", "")
+		diffEquation = self.tokenize(diffEquation)
+
+		outputQueue = []
+		operatorStack = []
+		for value in diffEquation:
+			if self.isfloat(value) or value == "x":
+				outputQueue.append(value)
+			elif value == "(":
+				operatorStack.append(value)
+
+			elif value == ")":
+				while operatorStack[-1] != "(":
+					assert (len(operatorStack) != 0)
+					outputQueue.append(operatorStack.pop())
+				assert (operatorStack[-1] == "(")
+				operatorStack.pop()
+			elif value in self.operations:
+
+				while (operatorStack and operatorStack[-1] != "("
+				       and self.precedence(operatorStack[-1]) >= self.precedence(value)):
+					outputQueue.append(operatorStack.pop())
+				operatorStack.append(value)
+		while operatorStack:
+			outputQueue.append(operatorStack.pop())
+
+		return outputQueue
+
+
+class ASTGraph:
+	def __init__(self):
+		self.operations = ["+", "-", "/", "*", "^"]
+
+	def checkForOperators(self, queue):
+		for q in queue:
+			if q in self.operations:
+				return True
+		return False
+
+	def returnOperatorName(self, operator):
+		match operator:
+			case "+":
+				return "ADD"
+			case "-":
+				return "SUB"
+			case "*":
+				return "MUL"
+			case "/":
+				return "DIV"
+			case "^":
+				return "POW"
+		return "UNK"
+
+	def getAST(self, shuntyardresult):
+		shuntres = shuntyardresult
+		graph = nx.DiGraph()
+
+		while self.checkForOperators(shuntres):
+			counter = 0
+			while shuntres[counter] not in self.operations:
+				counter += 1
+			print(f"Stopped @: {counter}")
+
+			if (counter - 2 >= 0 and ShuntingYard.isValue(shuntres[counter - 1]) and ShuntingYard.isValue(shuntres[counter - 2])):
+				node_name = self.returnOperatorName(shuntres[counter]) + "_" + ''.join(
+					random.choices(string.ascii_uppercase +
+					               string.digits, k=3))
+				graph.add_edge(str(shuntres[counter - 2]), node_name)
+				graph.add_edge(str(shuntres[counter - 1]), node_name)
+				print(f"{str(shuntres[counter - 2])} --> {node_name}")
+				print(f"{str(shuntres[counter - 1])} --> {node_name}")
+
+				for _ in range(3):
+					shuntres.pop(counter - 2)
+				shuntres.insert(counter - 2, node_name)
+
+		return graph
+
+
--- shuntingyardalg.py
+++ shuntingyardalg.py
@@ -1,29 +1,37 @@
 import random, string
 import networkx as nx
 from matplotlib import pyplot as plt
+import re
+
+import CalCoolUs.preprocess
 
 graph = nx.DiGraph()
-
-
-diffEquation = "( (( x^ 2)) +6 * x) * ( 4 + 5 / x )"
-diffEquation = diffEquation.replace(" ", "")
-
-
-def tokenize(string):
-    string = string.replace(" ", "")
-    array = []
-    for c in string:
-        array.append(c)
-    return array
-
-diffEquation = tokenize(diffEquation)
+operations = ["+", "-", "/", "*", "^"]
 
 def isfloat(number):
-    try: 
+    try:
         float(number)
         return True
     except:
         return False
+
+diffEquation = "( (( x^ -200.31419)) +66 * x) * ( 74 + 75 / x )"
+diffEquation = diffEquation.replace(" ", "")
+
+
+
+def tokenize(string):
+    r = [""]
+    for i in diffEquation:
+        if (i.isdigit() or i == ".") and isfloat(r[-1]):
+            r[-1] = r[-1] + i
+        else:
+            r.append(i)
+    return r[1:]
+
+diffEquation = tokenize(diffEquation)
+
+
 
 def isValue(number):
     return isfloat(number) or number == 'x' or isinstance(number, str)
@@ -58,7 +66,7 @@ def precedence(operator):
         case "^":
             return 3
     return 0
-operations = ["+", "-", "/", "*", "^"]
+
 for value in diffEquation:
     if isfloat(value) or value == "x":
         outputQueue.append(value)
@@ -80,7 +88,23 @@ for value in diffEquation:
 while operatorStack:
     outputQueue.append(operatorStack.pop())
 
-print(outputQueue)
+from CalCoolUs.preprocess import ShuntingYard
+
+myshunt = ShuntingYard()
+
+shuntres = myshunt.getPostfix("( (( x^ -200.31419)) +66 * x) * ( 74 + 75 / x )")
+assert myshunt.getPostfix("( (( x^ -200.31419)) +66 * x) * ( 74 + 75 / x )") == outputQueue
+
+from CalCoolUs.preprocess import ASTGraph
+
+myASTGraph = ASTGraph()
+newGraph = myASTGraph.getAST(shuntres)
+pos = nx.spring_layout(graph, scale=3)
+nx.draw_networkx(graph, pos=pos, with_labels=True)
+# plt.savefig("fig.png")
+plt.show(bbox_inches='tight')
+
+exit(4)
 
 def checkForOperators(queue):
     for q in queue:
@@ -97,7 +121,7 @@ while checkForOperators(outputQueue):
 
     if (counter - 2 >= 0 and isValue(outputQueue[counter-1]) and isValue(outputQueue[counter-2])):
         node_name = returnOperatorName(outputQueue[counter]) + "_" + ''.join(random.choices(string.ascii_uppercase +
-                                                                 string.digits, k=7))
+                                                                 string.digits, k=3))
         graph.add_edge(str(outputQueue[counter-2]), node_name)
         graph.add_edge(str(outputQueue[counter-1]), node_name)
         print(f"{str(outputQueue[counter - 2])} --> {node_name}")
@@ -109,7 +133,7 @@ while checkForOperators(outputQueue):
 
     print(outputQueue)
 
-pos = nx.nx_pydot.pydot_layout(graph)
+pos = nx.spring_layout(graph, scale=3)
 nx.draw_networkx(graph, pos=pos, with_labels=True)
 # plt.savefig("fig.png")
 plt.show(bbox_inches='tight')
